a198cb5318b1916d33faf806d62c11697ea44d08
David Wittenbrink <david.wittenbrink@gmail.com>
2016-11-11 00:31:05 +0100

    Implemented most important cli arg parsing. Improved docs. Switched from classification score to an error rate

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,67 @@
 import java.io.{BufferedWriter, File, FileWriter, IOException}
 
+import breeze.linalg.DenseVector
+import breeze.plot._
+
+case class Config(ploterrors: String = null,
+                  storesnapshot: String = null,
+                  hiddenlayers: Seq[Int] = Seq(30),
+                  epochs: Int = 50,
+                  learningrate: Double = 0.3,
+                  batchsize: Int = 10)
+
+
 /** A neural network designed to recognize handwritten digits.
   * The learning data is taken from the MNIST dataset.
   */
 object Main {
+
+  def savePlots(filename: String, trainingErrors: List[Double], testErrors: List[Double]) = {
+    val trainingErrorsVector = DenseVector[Double](trainingErrors.toArray)
+    val testErrorsVector = DenseVector[Double](testErrors.toArray)
+
+    val f = Figure()
+    val p = f.subplot(0)
+    p += plot(DenseVector[Double]((1 to trainingErrorsVector.length).map(_.toDouble).toArray), trainingErrorsVector, name="Training Error")
+    p += plot(DenseVector[Double]((1 to testErrorsVector.length).map(_.toDouble).toArray), testErrorsVector, name="Test Error")
+
+    p.legend = true
+    p.xlabel = "SGD epochs"
+    p.ylabel = "Error"
+    f.saveas(filename)
+  }
+
+  def parseCommandLineArgs(args: Array[String]): Config = {
+    val parser = new scopt.OptionParser[Config]("Neural network for handwritten digit classification") {
+      opt[String]('p', "ploterrors").valueName("<file>").
+        action( (x, c) => c.copy(ploterrors = x) ).
+        text("Plots a graph of training and testing errors to the specified location (in png format).")
+
+      opt[String]('s', "storesnapshot").valueName("<file>").
+        action( (x, c) => c.copy(storesnapshot = x) ).
+        text("Stores a snapshot of the trained net to the specified location (in JSON format).")
+
+      opt[Seq[Int]]('h', "hiddenlayers").valueName("<nr of neurons in 1. hidden layer>,<nr of neurons in 2. hidden layer>").
+        action( (x, c) => c.copy(hiddenlayers = x) ).
+        text("Use this to store a snapshot of the trained network in JSON format.")
+
+      opt[Int]('e', "epochs").valueName("<nr of epochs>").action( (x,c) =>
+        c.copy(epochs = x) ).text("The number of epochs for stochastic gradient descent")
+
+      opt[Double]('l', "learningrate").valueName("<learning rate>").action( (x,c) =>
+        c.copy(learningrate = x) ).text("The learning rate for gradient descent (decimal)")
+
+      opt[Int]('b', "batchsize").valueName("<batchsize").action( (x,c) =>
+        c.copy(epochs = x) ).text("The batchsize for stochastic gradient descent")
+
+      help("help").text("prints this usage text")
+
+    }
+    parser.parse(args, Config()) match {
+      case Some(config) => config
+      case None => null
+    }
+  }
 
   /** Writes a string to the given filename
     * @param filename The path of the file to write to
@@ -28,25 +86,35 @@ object Main {
     */
   def main(args: Array[String]): Unit = {
 
-    val nnLayers = List(784,30,10)
+    val parsedCommands = parseCommandLineArgs(args)
+    if (parsedCommands == null) return
+
+    val nnLayers = 784 +: parsedCommands.hiddenlayers.toList :+ 10
     val (weights, biases) = NeuralNetClassifier.generateRandomNetConfig(nnLayers)
 
     val (trainX, trainY) = MNIST.getTrainingSet
-    val (trainedWeights, trainedBiases) = NeuralNetClassifier.sgd(weights, biases, 10, 50, 0.3, trainX, trainY)
-
-    println("Training score:")
-    println(NeuralNetClassifier.calcClassificationScore(trainedWeights, trainedBiases, trainX, trainY))
-
-    println("Test score:")
     val (testX, testY) = MNIST.getTestSet
-    println(NeuralNetClassifier.calcClassificationScore(trainedWeights, trainedBiases, testX, testY))
 
-    if (args.length > 0) {
+    val result = NeuralNetClassifier.sgd(weights, biases, parsedCommands.batchsize,
+                                         parsedCommands.epochs, parsedCommands.learningrate,
+                                         trainX, trainY, testX, testY)
+    val (trainedWeights, trainedBiases, trainingErrors, testErrors) = result
+
+    println("Final training error:")
+    println(NeuralNetClassifier.calcClassificationError(trainedWeights, trainedBiases, trainX, trainY))
+
+    println("Final test error:")
+    println(NeuralNetClassifier.calcClassificationError(trainedWeights, trainedBiases, testX, testY))
+
+    if (parsedCommands.storesnapshot != null) {
       try {
-        writeStringToFile(args.head, NeuralNetClassifier.getJsonSnapshot(trainedWeights, trainedBiases))
+        writeStringToFile(parsedCommands.storesnapshot, NeuralNetClassifier.getJsonSnapshot(trainedWeights, trainedBiases))
       } catch {
         case ioe: IOException => println("Error writing to File.")
       }
+    }
+    if (parsedCommands.ploterrors != null) {
+      savePlots(parsedCommands.ploterrors, trainingErrors, testErrors)
     }
   }
 }
