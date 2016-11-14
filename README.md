d7c22099127a284c80b8e8758194e7035ff774d9
David Wittenbrink <david.wittenbrink@gmail.com>
2016-11-14 18:56:59 +0100

    Added command line options, documentation and updated read.

--- README.md
+++ README.md
@@ -1,32 +1,41 @@
 # Scala handwritten digit recognition
 
+For detailed documentation, visit the project's github site at
+https://davidwittenbrink.github.io/nn-handwritten-digit-recognition
+
 A scala neural network implementation used to correctly label
 handwritten digits. The network is trained using the MNIST
 dataset.
 
+The program offers options to save and load snapshot files of networks.
+These snapshot files are in the same format as the one used in
+Carmen Popoviciu's [awesome neural network
+implementation](https://github.com/CarmenPopoviciu/neural-net). 
+
+Additionally, the program supports adjusting parameters such as
+the learning rate, batch size for SGD or the number of hidden
+layers. You can see all command line options by running the program
+with '--help' as argument:
+```
+  -p, --ploterrors <file>  If specified, plots a graph of training and testing errors to the given location (in png format). E.g. '~/plot.png'.
+  -s, --storesnapshot <file>
+                           If specified, stores a snapshot of the trained net to the given location (in JSON format). E.g '/snaps/snapshot.json'.
+  -c, --configfromsnapshot <file>
+                           If specified, loads the network configuration (weights/bias) from the given snapshot file.
+  -h, --hiddenlayers <nr of neurons in 1. hidden layer>,<nr of neurons in 2. hidden layer>
+                           If specified, stores a snapshot of the trained network in JSON format to the given location. E.g. 'trainedNetwork.json'.
+  -e, --epochs <nr of epochs>
+                           The number of epochs for stochastic gradient descent.
+  -l, --learningrate <learning rate>
+                           The learning rate for gradient descent (decimal).
+  -b, --batchsize <batchsize
+                           The batchsize for stochastic gradient descent.
+  --help                   Prints this usage text.
+```
 As this is a learning project, the
 [NeuralNetClassifier object](/src/main/scala/NeuralNetClassifier.scala)
 is written in a functional manner.
-For an example how the classifier is used, see
-[the main function of this project](/src/main/scala/Main.scala).
-
-If the program is executed with a filepath as argument, it will
-save a snapshot of the trained network configuration to this
-location. The format of the JSON file is the same
-Carmen Popoviciu uses in her JavaScript [neural network
-implementation](https://github.com/CarmenPopoviciu/neural-net) 
-(see her repository for a sample).
-
-The [NeuralNetClassifier object](/src/main/scala/NeuralNetClassifier.scala)
-also offers a function to generate a network configuration from a given
-JSON snapshot. As an example, a configuration achieving a 77% test score
-is given in the [77.json](77.json) file.
-It was generated using
-* 784 input neurons, 1 hidden layer with 30 neurons, 10 output neurons
-* Stochastic gradient descent
-  * batch size 10
-  * learning rate 0.3
-  * 50 epochs
-  
-For more detailed documentation visit the project's github site at
-https://davidwittenbrink.github.io/nn-handwritten-digit-recognition
+If you are interested in using the classifier for other problems,
+you can check
+[the main function of this project](/src/main/scala/Main.scala)
+to see how to create and train a network.
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,20 +3,72 @@ import java.io.{BufferedWriter, File, FileWriter, IOException}
 import breeze.linalg.DenseVector
 import breeze.plot._
 
-case class Config(ploterrors: String = null,
-                  storesnapshot: String = null,
-                  hiddenlayers: Seq[Int] = Seq(30),
-                  epochs: Int = 50,
-                  learningrate: Double = 0.3,
-                  batchsize: Int = 10)
+
+/** A class holding the parsed command line arguments. It is populated by the scopt command line
+  * option parser.
+  *
+  * @param ploterrors The destination where the error plot will be stored. E.g. "/plots/errors.png"
+  * @param storesnapshot The destination where the created JSON snapshot will be stored. E.g. "~/snap.json"
+  * @param configfromsnapshot The file path to the JSON snapshot that will be parsed as the starting weights/bias
+  *                           config for the network.
+  * @param hiddenlayers A sequence which specifies the number of neurons per hidden layer. E.g. Seq(30, 20) would
+  *                     result in a network with two hidden layers, one with 30 and one with 20 neurons.
+  * @param epochs The number of epochs to run SGD
+  * @param learningrate The learning rate for SGD
+  * @param batchsize The batchsize for SGD
+  * @see https://github.com/scopt/scopt
+  */
+case class CliArgs(ploterrors: String = null,
+                   storesnapshot: String = null,
+                   configfromsnapshot: String = null,
+                   hiddenlayers: Seq[Int] = Seq(30),
+                   epochs: Int = 50,
+                   learningrate: Double = 0.3,
+                   batchsize: Int = 10)
 
 
-/** A neural network designed to recognize handwritten digits.
+/** An implementation of a neural network to recognize handwritten digits.
   * The learning data is taken from the MNIST dataset.
+  *
   */
 object Main {
 
-  def savePlots(filename: String, trainingErrors: List[Double], testErrors: List[Double]) = {
+  /** Return a network configuration either parsed from a snapshot file or created randomly.
+    *
+    * @param parsedCliArgs The cli arguments parsed by scopt.
+    *                      If parsedCliArgs.configFromSnapshot is null, no file was specified
+    * @return A tuple of a list of weights and a list of biases.
+    *         If config file was specified, it will be parsed and returned. If not, a random network config is returned.
+    * */
+  def loadNetConfigRandomlyOrFromFile(parsedCliArgs: CliArgs) = {
+    val nnLayers = 784 +: parsedCliArgs.hiddenlayers.toList :+ 10
+
+    if (parsedCliArgs.configfromsnapshot == null) {
+      NeuralNetClassifier.generateRandomNetConfig(nnLayers)
+    }
+    else {
+      try {
+        val source = io.Source.fromFile(parsedCliArgs.configfromsnapshot)
+        val lines = try source.getLines mkString "\n" finally source.close()
+        source.close()
+        NeuralNetClassifier.getConfigFromSnapshot(lines)
+      } catch {
+        case e: IOException => {
+          println("Error loading file. Using random config.")
+          NeuralNetClassifier.generateRandomNetConfig(nnLayers)
+        }
+      }
+    }
+  }
+
+  /** Creates and saves a plot of the training and test errors at the position specified via
+    * the CLI arguments.
+    *
+    * @param cliArgs The parsed CLI arguments.
+    * @param trainingErrors The list of training errors
+    * @param testErrors The list of test errors.
+    * */
+  def savePlots(cliArgs: CliArgs, trainingErrors: List[Double], testErrors: List[Double]) = {
     val trainingErrorsVector = DenseVector[Double](trainingErrors.toArray)
     val testErrorsVector = DenseVector[Double](testErrors.toArray)
 
@@ -25,39 +77,51 @@ object Main {
     p += plot(DenseVector[Double]((1 to trainingErrorsVector.length).map(_.toDouble).toArray), trainingErrorsVector, name="Training Error")
     p += plot(DenseVector[Double]((1 to testErrorsVector.length).map(_.toDouble).toArray), testErrorsVector, name="Test Error")
 
+    p.title = s"Test err=${testErrors.last.toString.substring(0, 4)} | " +
+              s"Training err=${trainingErrors.last.toString.substring(0, 4)} | " +
+              s"${cliArgs.epochs} epochs |" +
+              s"Learning rate=${cliArgs.learningrate} | Batch size=${cliArgs.batchsize} | " +
+              s"${cliArgs.hiddenlayers.length} hidden layers"
     p.legend = true
     p.xlabel = "SGD epochs"
     p.ylabel = "Error"
-    f.saveas(filename)
+    f.saveas(cliArgs.ploterrors)
   }
 
-  def parseCommandLineArgs(args: Array[String]): Config = {
-    val parser = new scopt.OptionParser[Config]("Neural network for handwritten digit classification") {
+
+  def parseCommandLineArgs(args: Array[String]): CliArgs = {
+    val parser = new scopt.OptionParser[CliArgs]("Neural network for handwritten digit classification") {
       opt[String]('p', "ploterrors").valueName("<file>").
         action( (x, c) => c.copy(ploterrors = x) ).
-        text("Plots a graph of training and testing errors to the specified location (in png format).")
+        text("If specified, plots a graph of training and testing errors to the given " +
+             "location (in png format). E.g. '~/plot.png'.")
 
       opt[String]('s', "storesnapshot").valueName("<file>").
         action( (x, c) => c.copy(storesnapshot = x) ).
-        text("Stores a snapshot of the trained net to the specified location (in JSON format).")
+        text("If specified, stores a snapshot of the trained net to the given location (in JSON format). " +
+             "E.g '/snaps/snapshot.json'.")
+
+      opt[String]('c', "configfromsnapshot").valueName("<file>").
+        action( (x, c) => c.copy(configfromsnapshot = x) ).
+        text("If specified, loads the network configuration (weights/bias) from the given snapshot file.")
 
       opt[Seq[Int]]('h', "hiddenlayers").valueName("<nr of neurons in 1. hidden layer>,<nr of neurons in 2. hidden layer>").
         action( (x, c) => c.copy(hiddenlayers = x) ).
-        text("Use this to store a snapshot of the trained network in JSON format.")
+        text("If specified, stores a snapshot of the trained network in JSON format to the given location. E.g. 'trainedNetwork.json'.")
 
       opt[Int]('e', "epochs").valueName("<nr of epochs>").action( (x,c) =>
-        c.copy(epochs = x) ).text("The number of epochs for stochastic gradient descent")
+        c.copy(epochs = x) ).text("The number of epochs for stochastic gradient descent.")
 
       opt[Double]('l', "learningrate").valueName("<learning rate>").action( (x,c) =>
-        c.copy(learningrate = x) ).text("The learning rate for gradient descent (decimal)")
+        c.copy(learningrate = x) ).text("The learning rate for gradient descent (decimal).")
 
       opt[Int]('b', "batchsize").valueName("<batchsize").action( (x,c) =>
-        c.copy(epochs = x) ).text("The batchsize for stochastic gradient descent")
+        c.copy(batchsize = x) ).text("The batchsize for stochastic gradient descent.")
 
-      help("help").text("prints this usage text")
+      help("help").text("Prints this usage text.")
 
     }
-    parser.parse(args, Config()) match {
+    parser.parse(args, CliArgs()) match {
       case Some(config) => config
       case None => null
     }
@@ -77,11 +141,12 @@ object Main {
   /** The main functions constructs a network with 784 input neurons, 30 neurons in the only hidden layer
     * and 10 output neurons. The network is then trained using Stochastic Gradient Descent using a batch
     * size of 10, a learning rate of 0.3 and 50 epochs. Finally, the test and training score of the network
-    * is printed to the command line. The network configuration can be stored as a JSON file in the same format
-    * which Carmen Popoviciu uses in her JavaScript NN implementation.
+    * is printed to the command line.
     *
-    * @param args The arguments list. The first argument, if provided, will be used to store a snapshot of the network
-    *             to.
+    * Multiple command line options allow to adjust this configuration and also to store the trained network to disk
+    * or load a previously trained network. For a detailed list of options, run the program with "--help".
+    *
+    * @param args The arguments list. For a detailed list of options, run the program with "--help"
     * @see [[https://github.com/CarmenPopoviciu/neural-net]]
     */
   def main(args: Array[String]): Unit = {
@@ -89,8 +154,7 @@ object Main {
     val parsedCommands = parseCommandLineArgs(args)
     if (parsedCommands == null) return
 
-    val nnLayers = 784 +: parsedCommands.hiddenlayers.toList :+ 10
-    val (weights, biases) = NeuralNetClassifier.generateRandomNetConfig(nnLayers)
+    val (weights, biases) = loadNetConfigRandomlyOrFromFile(parsedCommands)
 
     val (trainX, trainY) = MNIST.getTrainingSet
     val (testX, testY) = MNIST.getTestSet
@@ -114,7 +178,7 @@ object Main {
       }
     }
     if (parsedCommands.ploterrors != null) {
-      savePlots(parsedCommands.ploterrors, trainingErrors, testErrors)
+      savePlots(parsedCommands, trainingErrors, testErrors)
     }
   }
 }
